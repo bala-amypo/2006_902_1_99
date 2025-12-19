35d4ca3fd97e6e2b7f30d5d46c6e83ca04da1c5a
bala-amypo <bala@amypo.in>
2025-12-19 06:20:52 +0000

    User commit

--- src/main/java/com/example/demo/service/AssetDisposalService.java
+++ src/main/java/com/example/demo/service/AssetDisposalService.java
@@ -7,13 +7,3 @@ public interface AssetDisposalService {
     AssetDisposal approveDisposal(Long disposalId, Long adminId);
 }
 
-8. AssetLifecycleEventService.java
-package com.example.demo.service;
-
-import com.example.demo.entity.AssetLifecycleEvent;
-import java.util.List;
-
-public interface AssetLifecycleEventService {
-    AssetLifecycleEvent logEvent(Long assetId, AssetLifecycleEvent event);
-    List<AssetLifecycleEvent> getEventsForAsset(Long assetId);
-}
